9436d19e0bf298538e1bd11a5069e10a52dba832
lokki <44494883+luckylokki@users.noreply.github.com>
2023-03-27 11:25:07 +0300

    media folder check

--- CIMS/urls.py
+++ CIMS/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('staff.urls')),
-    path('',include('inventory.urls'))
+    path('',include('inventory.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
